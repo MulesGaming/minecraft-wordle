33ed278659058565f2a785296120b21d69873c0a
Mules Gaming <battledroidmule@gmail.com>
2023-04-11 10:15:57 -0500

    Fixed yellows AGIAN & fixed loss banner

--- scripts/main.js
+++ scripts/main.js
@@ -234,9 +234,9 @@ function checkForCorrectLetters () {
       tile[i].style.backgroundColor="var(--correct-color)"
       allreadyChecked[i] = `${tileText[i]}`;
     }else if (word.includes(tileText[i])){
-      if (!(countArrayValues(allreadyChecked, tileText[i]) >= countArrayValues(word, tileText[i])) && (fullUserWord.split(tileText[i]).length - 1 <= countArrayValues(word, tileText[i]))) {
-        tile[i].style.backgroundColor="var(--semicorrect-color)"
-        allreadyChecked[i] = `${tileText[i]}`;
+      if (!(countArrayValues(allreadyChecked, tileText[i]) >= countArrayValues(word, tileText[i])) && (countArrayValues(allreadyChecked, tileText[i]) - 1 <= countArrayValues(word, tileText[i])) && !(tileText[i] === tileText[i + 1] && tileText[i + 1] === word[i + 1]) && !(tileText[i] === tileText[i + 2] && tileText[i + 2] === word[i + 2]) && !(tileText[i] === tileText[i + 3] && tileText[i + 3] === word[i + 3]) && !(tileText[i] === tileText[i + 4] && tileText[i + 4] === word[i + 4]) && !(tileText[i] === tileText[i + 5] && tileText[i + 5] === word[i + 5])) {
+            tile[i].style.backgroundColor="var(--semicorrect-color)"
+            allreadyChecked[i] = `${tileText[i]}`;
       }else{
         tile[i].style.backgroundColor="var(--incorrect-color)"
         allreadyChecked[i] = `${tileText[i]}`;  
